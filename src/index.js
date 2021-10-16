78f46ea8049f04d0a6567ab557bbfcc711b2478b
ReneP8 <Rene-Baving@web.de>
2021-10-16 02:05:25 +0200

    integrated witzapi.de; made simple call on command

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
+const fetch = require('node-fetch');
 const { Client, Intents } = require('discord.js');
+
 
 require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES] });
+
+async function getRandomJoke() {
+    try {
+        const response = await fetch('https://witzapi.de/api/joke');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 
 client.once('ready', () => {
     console.log('Beep beep! I am ready!');
+});
+
+client.on('messageCreate', async(msg) => {
+    if (msg.content === '!joke') {
+        await getRandomJoke().then(joke => {
+            msg.reply(joke[0].text)
+                .then(() => console.log(`Replied to message "${msg.content}"`))
+                .catch(console.error);
+        }).catch(error => console.log(error));
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
